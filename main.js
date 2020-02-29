42c47791a6c63da18bc0eb4e82b954b6f0b4b018
Karol Czyż <karol.czyz.97@gmail.com>
2020-03-01 00:59:01 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+class TypeWriter {
+  constructor(typewriterEl, greetings) {
+    this.element = typewriterEl
+    this.greetings = greetings
+    this.currentText = ''
+    this.counter = 0
+    this.deleting = false
+
+    this.type()
+  }
+
+  type = () => {
+    const currentWordIndex = this.counter % this.greetings.length
+    const currentWord = this.greetings[currentWordIndex]
+
+    let charDifference = this.deleting ? -1 : 1
+    this.currentText = currentWord.substring(0, this.currentText.length + charDifference);
+
+    this.element.innerHTML = `<span id="typewriter">${this.currentText}</span>`
+    document.title = this.currentText
+
+    let typingDelay = this.deleting ? 50 : 200
+
+    if (this.currentText === currentWord) {
+      this.deleting = true
+      typingDelay = 3000
+    } else if (this.currentText === '') {
+      this.deleting = false
+      this.counter++
+    }
+
+    setTimeout(this.type, typingDelay)
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const greetings = ['Dzień dobry!', 'Good morning!', 'Guten Tag!', 'Buenos Dias!']
+  const typewriterEl = document.getElementById('typewriter')
+
+  new TypeWriter(typewriterEl, greetings)
+})
